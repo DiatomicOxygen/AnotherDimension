e6bd2acd6d9c8cc5d2b90db8ab9d5746e32dae63
DiatomicOxygen <eoo20@stuy.edu>
2020-10-23 13:20:38 -0400

    Created Tester main function

--- Tester.java
+++ Tester.java
@@ -1,5 +1,8 @@
 public class Tester {
   public static void main(String[] args) {
-    
+    int[] a = new int[args.length];
+    for (int i = 0; i < args.length; i++) {
+      a[i] += Integer.parseInt(args[i]);
+    }
   }
 }
